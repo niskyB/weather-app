75773e1defda39f2a895407d14a4d5cf41be734d
Loc Nguyen Hoang <loc.nguyen@yocar.vn>
2024-03-26 12:05:17 +0700

    Update not found message

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -20,7 +20,7 @@ export const Input: React.FC<IProps> = ({ value, onChangeInput, error }) => {
         {error?.response?.status === 404 || error?.response?.status === 400 ? (
           <>
             <span className="text-sm text-red-500 font-medium">
-              City not found
+              City is not found
             </span>
           </>
         ) : (
